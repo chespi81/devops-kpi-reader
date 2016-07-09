06562810bdaefbefbdf998a3b4f58c0bf0e2caf1
rsanmartin <rsanmartin@tinet.cl>
2016-07-09 18:16:45 -0400

    Se soporta consulta paginada de proyectos.

--- bitbucket-cloud-api2/src/main/java/cl/tinet/devops/metrics/gen/bitbucket/api/ProjectsApi.java
+++ bitbucket-cloud-api2/src/main/java/cl/tinet/devops/metrics/gen/bitbucket/api/ProjectsApi.java
@@ -38,49 +38,78 @@ public class ProjectsApi {
     this.apiClient = apiClient;
   }
 
-  /**
-   * 
-   * 
-   * @param owner The team which owns the project. This can either be the &#x60;username&#x60; of the team or the &#x60;UUID&#x60; of the team (surrounded by curly-braces (&#x60;{}&#x60;)).  (required)
-   * @return PaginatedProjects
-   * @throws ApiException if fails to make API call
-   */
-  public PaginatedProjects teamsOwnerProjectsGet(String owner) throws ApiException {
-    Object localVarPostBody = null;
-    
-    // verify the required parameter 'owner' is set
-    if (owner == null) {
-      throw new ApiException(400, "Missing the required parameter 'owner' when calling teamsOwnerProjectsGet");
-    }
-    
-    // create path and map variables
-    String localVarPath = "/teams/{owner}/projects/".replaceAll("\\{format\\}","json")
-      .replaceAll("\\{" + "owner" + "\\}", apiClient.escapeString(owner.toString()));
+	/**
+	 * 
+	 * 
+	 * @param owner
+	 *            The team which owns the project. This can either be the
+	 *            &#x60;username&#x60; of the team or the &#x60;UUID&#x60; of
+	 *            the team (surrounded by curly-braces (&#x60;{}&#x60;)).
+	 *            (required)
+	 * @return PaginatedProjects
+	 * @throws ApiException
+	 *             if fails to make API call
+	 */
+	public PaginatedProjects teamsOwnerProjectsGet(String owner)
+			throws ApiException {
+		return teamsOwnerProjectsGet(owner, 1);
+	}
 
-    // query params
-    List<Pair> localVarQueryParams = new ArrayList<Pair>();
-    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
-    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+	/**
+	 * 
+	 * 
+	 * @param owner
+	 *            The team which owns the project. This can either be the
+	 *            &#x60;username&#x60; of the team or the &#x60;UUID&#x60; of
+	 *            the team (surrounded by curly-braces (&#x60;{}&#x60;)).
+	 *            (required)
+	 * @param page
+	 *            The page number.
+	 * @return PaginatedProjects
+	 * @throws ApiException
+	 *             if fails to make API call
+	 */
+	public PaginatedProjects teamsOwnerProjectsGet(String owner, int page)
+			throws ApiException {
+		Object localVarPostBody = null;
 
+		// verify the required parameter 'owner' is set
+		if (owner == null) {
+			throw new ApiException(400,
+					"Missing the required parameter 'owner' when calling teamsOwnerProjectsGet");
+		}
 
-    
-    
-    final String[] localVarAccepts = {
-      "application/json"
-    };
-    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+		// create path and map variables
+		String localVarPath = "/teams/{owner}/projects/".replaceAll(
+				"\\{format\\}", "json").replaceAll("\\{" + "owner" + "\\}",
+				apiClient.escapeString(owner.toString()));
 
-    final String[] localVarContentTypes = {
-      "application/json"
-    };
-    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+		// query params
+		List<Pair> localVarQueryParams = new ArrayList<Pair>();
+		localVarQueryParams.add(new Pair("page", String.valueOf(page)));
+		Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+		Map<String, Object> localVarFormParams = new HashMap<String, Object>();
 
-    String[] localVarAuthNames = new String[] { "api_key", "oauth2", "basic" };
+		final String[] localVarAccepts = { "application/json" };
+		final String localVarAccept = apiClient
+				.selectHeaderAccept(localVarAccepts);
 
-    GenericType<PaginatedProjects> localVarReturnType = new GenericType<PaginatedProjects>() {};
-    return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
-      }
-  /**
+		final String[] localVarContentTypes = { "application/json" };
+		final String localVarContentType = apiClient
+				.selectHeaderContentType(localVarContentTypes);
+
+		String[] localVarAuthNames = new String[] { "api_key", "oauth2",
+				"basic" };
+
+		GenericType<PaginatedProjects> localVarReturnType = new GenericType<PaginatedProjects>() {
+		};
+		return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams,
+				localVarPostBody, localVarHeaderParams, localVarFormParams,
+				localVarAccept, localVarContentType, localVarAuthNames,
+				localVarReturnType);
+	}
+
+/**
    * 
    * 
    * @param owner The team which owns the project. This can either be the &#x60;username&#x60; of the team or the &#x60;UUID&#x60; of the team (surrounded by curly-braces (&#x60;{}&#x60;)).  (required)
